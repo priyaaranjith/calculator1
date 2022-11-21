ba87dc8e242c52b5866083806e58c882ef906849
priyaaranjith <ranjithpriya307@gmail.com>
2022-11-21 11:00:56 +0530

    addition

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
-public class Main {
-    public static void main(String[] args) {
+public class Main
+{
+    public static double sum(double i, double j) {
+        double sum = i + j;
+        return sum;
+    }
+    public static void main(String[] args)
+        {
         System.out.println("calculate");
     }
 }
